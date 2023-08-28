86d21d3561fb3280b2f3a3e80afd82498b723c3b
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-28 13:47:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const out_div = document.getElementById("output");
+async function getOutput(){
+    const input_text = document.getElementById("text");
+    const delay = document.getElementById("delay").value;
+	 setTimeout(() =>{
+		display();
+		
+	},delay*1000)
+}
+
+function display(){
+    const input_text = document.getElementById("text").value;
+	const val = document.createElement("h1");
+	val.innerText = input_text;
+	out_div.append(val);
+	
+}
+
+
