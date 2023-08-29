30b48dae84df7f68d9cec952bbf6616a8afa0ec0
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-29 14:16:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-//your JS code here. If required.
 const out_div = document.getElementById("output");
-async function getOutput(){
-    const input_text = document.getElementById("text");
-    const delay = document.getElementById("delay").value;
-	 setTimeout(() =>{
-		display();
-		
-	},delay*1000)
-}
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
 
-function display(){
-    const input_text = document.getElementById("text").value;
-	const val = document.createElement("h1");
-	val.innerText = input_text;
-	out_div.append(val);
+btn.addEventListener("click",()=>{
+	const msg = text.value;
+	const delay_time = Number(delay.value);
+    console.log(delay_time);
+	if(msg && delay_time>0){
+		call(msg,delay_time);
+	}
+})
+
+async function call(msg,delay_time){
+	let x = await new Promise((resolve) =>{
+		setTimeout(() =>{
+			resolve(msg);
+		},delay_time)
+	})
+    out_div.innerText = x;
 	
 }
-
 
